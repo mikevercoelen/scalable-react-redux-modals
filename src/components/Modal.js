7b1d6682c08c5b5af6ba17ce6c13610ca5f1a383
Mike Vercoelen <mike.vercoelen@gmail.com>
2017-02-15 21:36:21 +0100

    Added styled-components to style

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,58 @@
 import React, { Component, PropTypes } from 'react';
+import styled from 'styled-components';
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  z-index: 1000;
+  background-color: rgba(0, 0, 0, .65);
+`;
+
+const Content = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 10000;
+  overflow: auto;
+  text-align: center;
+  cursor: pointer;
+  overflow-scrolling: touch;
+  padding: 4px;
+
+  &:after {
+    vertical-align: middle;
+    display: inline-block;
+    height: 100%;
+    margin-left: -.05em;
+    content: '';
+  }
+`;
+
+const Dialog = styled.div`
+  position: relative;
+  outline: 0;
+  width: 100%;
+  background: white;
+  display: inline-block;
+  vertical-align: middle;
+  box-sizing: border-box;
+  max-width: 520px;
+`;
+
+const Header = styled.div`
+  padding: 16px 8px 8px 8px
+`;
+
+const Body = styled.div`
+  padding-bottom: 16px
+`;
 
 export default class Modal extends Component {
   static propTypes = {
@@ -46,18 +100,18 @@ export default class Modal extends Component {
   render () {
     return (
       <div className='modal'>
-        <div className='modal__overlay' />
-        <div className='modal__content'>
-          <div className='modal__dialog'>
-            <div className='modal__header'>
+        <Overlay />
+        <Content>
+          <Dialog>
+            <Header>
               {this.title}
               {this.close}
-            </div>
-            <div className='modal__body'>
+            </Header>
+            <Body>
               {this.props.children}
-            </div>
-          </div>
-        </div>
+            </Body>
+          </Dialog>
+        </Content>
       </div>
     );
   }
