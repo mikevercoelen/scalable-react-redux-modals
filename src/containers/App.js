0b1f2ff11d979848fafb5663cc097f2161af14db
Mike Vercoelen <mike.vercoelen@gmail.com>
2017-02-17 12:33:56 +0100

    Added global styling to make things look better

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 import ModalRoot from './ModalRoot';
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  body {
+    margin: 0;
+    -webkit-font-smoothing: antialiased;
+    font-family: sans-serif;
+  }
+`;
 
 export default class Application extends Component {
   render () {
